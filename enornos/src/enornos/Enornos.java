a8b9e2a829a1670cc9e7f20f81e482a4ea6760f3
dam1 <dam1@1DAM-01.IES.QUEVEDO-AD>
2016-01-20 13:12:41 +0100

    segunda version

--- enornos/src/enornos/Enornos.java
+++ enornos/src/enornos/Enornos.java
@@ -13,7 +13,7 @@ public class Enornos {
 
     public static void main(String[] args) {
         System.out.println("hola mundo");
-        System.out.println("dasdwr qweqwe");
+        System.out.println("dasdwrwr234weqwe");
     }
     
 }

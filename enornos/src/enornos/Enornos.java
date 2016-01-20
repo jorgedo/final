1974a289f0dc7d1ee721f34e79368cfafe305073
dam1 <dam1@1DAM-01.IES.QUEVEDO-AD>
2016-01-20 13:04:34 +0100

    primera version

---
+++ enornos/src/enornos/Enornos.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enornos;
+
+/**
+ *
+ * @author dam1
+ */
+public class Enornos {
+
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        System.out.println("dasdwr ");
+    }
+    
+}
